e2eab8f7f142ed7563733627db9fb97c94801c87
Wolford027 <joshuacarbajosa74@gmail.com>
2024-03-04 13:25:33 +0800

    Changed

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,31 +1,35 @@
-import { Avatar, Button, Grid,Paper, TextField } from '@mui/material'
-import Logo from '../assets/B.png'
-import * as React from 'react'
+import { Avatar, Button, Grid, Paper, TextField } from '@mui/material';
+import Logo from '../assets/B.png';
+import * as React from 'react';
 
 export default function Login() {
-
-    const paperStyle={padding:20, height:'70vh', width:280,  margin:"60px auto"}
-    const textStyle={margin:"10px 0 0 30px"}
-    const loginStytle={borderRadius:5, margin:"10px 0 0 100px"}
-    const gridStyle={margin:30}
-    const headerStyle={margin:-10}
-
+  const paperStyle = {
+    padding: 20,
+    height: '80vh',
+    width: 320,
+    margin: '0 auto',
+    marginTop: '50px',
+    marginLeft: '960px',
+  };
+  const textStyle = { margin: '10px 0' };
+  const loginStyle = { borderRadius: 5, marginTop: 20 };
+  const headerStyle = { margin: 0 };
 
   return (
     <div>
-      <Grid>
+      <Grid container justifyContent="center">
         <Paper elevation={10} style={paperStyle}>
-            <Grid align='center' style={gridStyle}>
-            <Avatar alt='Logo'
-            src={Logo}
-            sx={{width:120, height:120}}></Avatar>
+          <Grid align="center">
+            <Avatar alt="Logo" src={Logo} sx={{ width: 120, height: 120 }} />
             <h2 style={headerStyle}>Login</h2>
-            </Grid>
-            <TextField label='Username' placeholder='Enter Username' style={textStyle}/>
-            <TextField label='Password' placeholder='Enter Password' type='password' style={textStyle}/>
-            <Button type='submit' color='primary' variant='contained' style={loginStytle} href='dashboard'>Login</Button>
+          </Grid>
+          <TextField label="Username" placeholder="Enter Username" style={textStyle} fullWidth />
+          <TextField label="Password" placeholder="Enter Password" type="password" style={textStyle} fullWidth />
+          <Button type="submit" color="primary" variant="contained" style={loginStyle} href="dashboard" fullWidth>
+            Login
+          </Button>
         </Paper>
       </Grid>
     </div>
-  )
+  );
 }
