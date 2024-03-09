b0d599e34ea627ae22b28b29dcd74e2df7b1558c
Wolford027 <joshuacarbajosa74@gmail.com>
2024-03-09 14:13:37 +0800

    Bago

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './Pages/Dashboard';
 import Login from './Pages/Login';
-import EmployeeList from './Pages/EmployeeList';
+import EmployeeList from './Employee/EmployeeList';
+import EmployeeAttendance from './Employee/EmployeeAttendance';
+import EmployeeLoan from './Employee/EmployeeLoan';
  
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/employee-list' element={<EmployeeList/>} />
+        <Route path='/employee-attendance' element={<EmployeeAttendance/>} />
+        <Route path='/employee-loan' element={<EmployeeLoan/>} />
       </Routes>
     </BrowserRouter>
   );
