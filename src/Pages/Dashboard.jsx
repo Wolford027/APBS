6785778c6356a1fd9c204113a79a684f5a9b7f71
Wolford027 <joshuacarbajosa74@gmail.com>
2024-04-02 18:49:17 +0800

    tabled

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,60 +1,14 @@
-import {React, useState} from 'react'
-import {Box, ThemeProvider, Grid, Typography, Avatar } from '@mui/material'
-import SideNav from '../Components/SideNav'
+import {React} from 'react'
 import Schedule from '../Components/Schedule'
-import Profile from '../Components/Profle'
+import Company from '../Components/Company'
 
 export default function Dashboard(){
-
-  const companyName = "Company Name"
-  const [showProfile, setShowProfile] = useState(false);
-
-  const handleAvatarClick = () => {
-    setShowProfile(true);
-  };
-
-  const handleCloseProfile = () => {
-    setShowProfile(false);
-  };
-
-
 
   return(
 
     <>
-    <Box sx={{display: "flex", marginTop:10, marginLeft:-10}}>
-      <SideNav/>
-      <ThemeProvider
-      theme={{
-        palette: {
-          primary: {
-            main: '#007FFF',
-            dark: '#0066CC',
-          },
-        },
-      }}
-    >
-      <Box
-        sx={{
-          width: 1450,
-          height: 2,
-          borderRadius: 1,
-          bgcolor: 'primary.main',
-        }}
-      />
-    </ThemeProvider>
-    <Grid container wrap="nowrap" spacing={2} sx={{marginLeft: -135, marginTop: -9, marginBottom: 15}}>
-          <Grid item>
-            <Avatar src="/broken-image.jpg" sx={{cursor: "pointer"}} onClick={handleAvatarClick}/>
-          </Grid>
-          <Grid item xs zeroMinWidth>
-            <Typography noWrap>Welcome, HR!</Typography>
-            <Typography noWrap>{companyName}</Typography>
-          </Grid>
-    </Grid>
+    <Company/>
     <Schedule/>
-    </Box>
-    {showProfile && <Profile onClose={handleCloseProfile} />}
     </>
   )
 }
