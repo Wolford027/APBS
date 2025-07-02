2331e99d3785dc50f3fc52346557358774ee9d73
Wolford027 <joshuacarbajosa74@gmail.com>
2025-07-02 08:01:58 +0800

    07-02-25

--- backend/main.js
+++ backend/main.js
@@ -50,6 +50,41 @@ app.get("/", (req, res) => {
   return res.json("BACKEND");
 });
 
+// To Login Users
+app.post("/login", (req, res) => {
+  const sql = "SELECT emp_id, role FROM users WHERE username = ? AND password = ?";
+  db.query(sql, [req.body.user, req.body.password], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    if (data.length > 0) {
+      const userRole = data[0].role;
+      const userId = data[0].emp_id;
+      return res.json({
+        message: "Log in Successfully",
+        role: userRole,
+        emp_id: userId
+      });
+    } else {
+      return res.json("No Record Found");
+    }
+  });
+});
+
+//Login History
+app.post('/login-history', (req, res) => {
+  const loginEvent = req.body;
+  const query = `INSERT INTO audit_trail (emp_id, date, role, action) VALUES (?, ?, ?, ?)`;
+  db.query(query, [loginEvent.emp_id, loginEvent.date, loginEvent.role, loginEvent.action], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Error storing login history' });
+    } else {
+      res.send({ message: 'Login history stored successfully' });
+    }
+  });
+});
+
 //Display All Employees
 app.get("/emp", (req, res) => {
   const sql = "SELECT * FROM emp_info WHERE is_archive = 0";
@@ -93,38 +128,103 @@ app.post('/upload/:id', upload.single('image'), (req, res) => {
   });
 });
 
-// To Login Users
-app.post("/login", (req, res) => {
-  const sql = "SELECT emp_id, role FROM users WHERE username = ? AND password = ?";
-  db.query(sql, [req.body.user, req.body.password], (err, data) => {
-    if (err) {
-      return res.json("Error");
-    }
-    if (data.length > 0) {
-      const userRole = data[0].role;
-      const userId = data[0].emp_id;
-      return res.json({
-        message: "Log in Successfully",
-        role: userRole,
-        emp_id: userId
+// INSERT EMPLOYEE INFO
+app.post('/AddEmp', (req, res) => {
+  const { surname, firstname, middlename, suffix, civilStatusId, sexId, citizenshipId, religionId, dateOfBirth, provinceOfBirth, municipalityOfBirth,
+    email, number, region, province, municipality, barangay, streetadd,
+    status, employmentType, position, ratetype, rateValue, department, datestart, dateend, sss, philHealth, hdmf, tin } = req.body;
+
+  const query = 'INSERT INTO emp_info (l_name, f_name, m_name, suffix, civil_status, sex, citizenship, religion, birthday, province_of_birth, city_of_birth, email, mobile_num, region, province, city, barangay, street, emp_status, emp_type, position,  rate_type, salary_rate, department, date_hired, date_end, tin_num, sss_num, philhealth_num, hdmf_num) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?)';
+  db.query(query, [surname, firstname, middlename, suffix, civilStatusId, sexId, citizenshipId, religionId, dateOfBirth, provinceOfBirth, municipalityOfBirth,
+    email, number, region.region_name, province, municipality, barangay, streetadd,
+    status, employmentType, position, ratetype, rateValue, department, datestart, dateend, sss, philHealth, hdmf, tin], (error, results) => {
+      if (error) {
+        console.error('Error inserting employee into database:', error); // Log error details
+        return res.status(500).json({ error: 'Failed to insert employee' });
+      }
+
+      res.status(200).json({ insertId: results.insertId });
+    });
+});
+
+// Add educational background
+app.post('/AddEducbg', (req, res) => {
+  const eduBgData = req.body; // Get the incoming data
+  console.log('Received educational background data:', eduBgData); // Log the incoming data
+
+  // Check if eduBgData is an array
+  if (!Array.isArray(eduBgData)) {
+    console.error('Expected eduBgData to be an array, but got:', typeof eduBgData);
+    return res.status(400).json({ error: 'Invalid data format for educational background' });
+  }
+
+  // Proceed with inserting data into the database
+  const queries = eduBgData.map(item => {
+    const query = 'INSERT INTO education_background (emp_id, school_uni_id, school_university, category, year) VALUES (?, ?, ?, ?, ?)';
+    return new Promise((resolve, reject) => {
+      db.query(query, [item.emp_id, item.school_uni_id, item.school_university, item.category, item.year], (error, results) => {
+        if (error) {
+          console.error('Error inserting educational background:', error);
+          return reject(error);
+        }
+        resolve(results);
       });
-    } else {
-      return res.json("No Record Found");
-    }
+    });
+  });
+
+  Promise.all(queries)
+    .then(() => res.status(200).json({ message: 'Educational background added successfully!' }))
+    .catch(error => {
+      console.error('Error in processing educational background:', error);
+      res.status(500).json({ error: 'Failed to add educational background' });
+    });
+});
+
+// INSERT EMPLOYEE EARNINGS ADDITIONAL BENIFITS
+app.post('/AddEmpBenefits', (req, res) => {
+  const benefitsData = req.body;
+
+  // Check if benefitsData is an array; if not, respond with an error
+  if (!Array.isArray(benefitsData)) {
+    console.error('Expected benefitsData to be an array, but got:', typeof benefitsData);
+    return res.status(400).json({ error: 'Invalid data format for benefits data' });
+  }
+  const queries = benefitsData.map(item => {
+    const query = 'INSERT INTO allowance_benefits (emp_id, allowance_name, allowance_value, allowance_type) VALUES (?, ?, ?, ?)';
+    return new Promise((resolve, reject) => {
+      db.query(query, [item.emp_id, item.name, item.value, item.allowanceType], (error, results) => {
+        if (error) {
+          console.error('Error inserting benefits data:', error);
+          return reject(error);
+        }
+        resolve(results);
+      });
+    });
+  });
+
+  Promise.all(queries)
+    .then(() => res.status(200).json({ message: 'Benefits added successfully!' }))
+    .catch(error => {
+      console.error('Error in processing benefits data:', error);
+      res.status(500).json({ error: 'Failed to add benefits' });
+    });
+});
+
+//Fetch Civil Status
+app.get("/cs", (req, res) => {
+  const sql = "SELECT * FROM civil_status";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
   });
 });
 
-//Login History
-app.post('/login-history', (req, res) => {
-  const loginEvent = req.body;
-  const query = `INSERT INTO audit_trail (emp_id, date, role, action) VALUES (?, ?, ?, ?)`;
-  db.query(query, [loginEvent.emp_id, loginEvent.date, loginEvent.role, loginEvent.action], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send({ message: 'Error storing login history' });
-    } else {
-      res.send({ message: 'Login history stored successfully' });
-    }
+//FETCH Sex thje Sex
+app.get("/sex", (req, res) => {
+  const sql = "SELECT * FROM sex";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
   });
 });
 
